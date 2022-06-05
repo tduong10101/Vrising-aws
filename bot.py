9c9804076fce3ad4d289d4b3b336e5f0834552da
Terry Duong <tduong10101@gmail.com>
2022-06-05 10:41:53 +1000

    added bot, consolidate vrisng

---
+++ bot.py
@@ -0,0 +1,43 @@
+import vrising
+import json
+from dotenv import dotenv_values
+from discord.ext import commands
+import discord
+
+config = dotenv_values(".env")
+
+TOKEN=config['DISCORD_TOKEN']
+PREFIX=config['PREFIX']
+
+bot = discord.Client()
+
+### PREFIX ###
+bot = commands.Bot(command_prefix=PREFIX)
+
+# succesfully message
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+### CALL ###
+#!info
+@bot.command(name='info', help='bot info')
+async def test(ctx):
+    response = 'coin-bot'
+    await ctx.send(response)
+
+#!rise
+@bot.command(name='rise')
+async def test(ctx, name, coins):
+    Message = vrising.start_server()
+    print(Message)
+    await ctx.send(Message)
+
+#!fall
+@bot.command(name='fall')
+async def test(ctx, name, coins):
+    Message = vrising.stop_server()
+    print(Message)
+    await ctx.send(Message)
