2fbef7c8ff393eda19eaba221ea17a759758f6d4
Terry Duong <tduong10101@gmail.com>
2023-07-29 00:43:16 +1000

    Update _provider.tf

--- terraform/_provider.tf
+++ terraform/_provider.tf
@@ -9,16 +9,12 @@ terraform {
         bucket = "tduong10101-terraform-state-bucket"
         key    = "terraform_state"
         region = "ap-southeast-2"
-        role_arn = "arn:aws:iam::069363837566:role/vrisng-github-terraform"
     }
 }
 
 # Configure the AWS Provider
 provider "aws" {
     region = "ap-southeast-2"
-    assume_role {
-        role_arn = "arn:aws:iam::069363837566:role/vrisng-github-terraform"
-    }
 }
 
 
