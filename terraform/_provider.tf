846679832b2b24df9543b235b5514d7f827459f8
Terry Duong <tduong10101@gmail.com>
2023-07-29 00:34:48 +1000

    Update _provider.tf

--- terraform/_provider.tf
+++ terraform/_provider.tf
@@ -9,6 +9,7 @@ terraform {
         bucket = "tduong10101-terraform-state-bucket"
         key    = "terraform_state"
         region = "ap-southeast-2"
+        role_arn = "arn:aws:iam::069363837566:role/vrisng-github-terraform"
     }
 }
 
